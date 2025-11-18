023460faf5abb132d9b0d2a65ed6971c8b7f7409
aironguimass <aironpablo@hotmail.com>
2025-11-18 17:22:04 +0000

    Update Copia.c
    atualizei o sistema para ter a opção de: 
    carregamento automático ao iniciar
    le equipas do ficheiro "equipe.txt" na inicialização (caso queira ler de outro ficheiro é só mudar o nome .txt)
    cria um id unico para cada time, independente se algum for excluido
    a opção salvar foi adaptada para com a carregar dados
    ajustes de bugs e algumas melhorias de fluxo.

--- LINUX/Copia/Copia/Copia.c
+++ LINUX/Copia/Copia/Copia.c
@@ -17,13 +17,23 @@ typedef struct
     char nome[50];
 }Equipa;
 
+
+typedef struct
+{
+    int id;
+    char Equipa[50];
+}Jogos;
+
+
 //Array de 50 times e contador de cadastros
 Equipa equipas[50];
 int totalEquipas = 0;
+int proximoID = 1;
 
+//função para salvar o projeto quando selecionarmos a opção 6 na main()
 void salvarDados()
 {
-    FILE* arquivo = fopen("equipe.txt", "w");
+    FILE* arquivo = fopen("equipas.txt", "w");  //abre o arquivo e escreve
     if (arquivo == NULL)
     {
         printf("                       ❌ ERRO AO CRIAR ARQUIVO!\n");
@@ -32,12 +42,69 @@ void salvarDados()
     for (int i = 0; i < totalEquipas; i++)
     {
         fprintf(arquivo, "%d | %s\n", equipas[i].id, equipas[i].nome);
-    }
+    }  
     fclose(arquivo);
 
-    printf("\n\n                        ✅ %d equipas salvas em 'equipe.txt'.\n\n", totalEquipas);
+    printf("\n\n                        ✅ %d equipas salvas em 'equipas.txt'.\n\n", totalEquipas);
     sleep(3); // Alterado de Sleep(3000) para sleep(3)
 }
+//função para carregar dados (é ativa automaticamente quando se inicia o programa na main())
+void carregarDados()
+{
+    FILE* arquivo = fopen("equipas.txt", "r"); //abre o arquivo e le
+    if (arquivo == NULL)
+    {
+        printf("                       ❌ FICHEIRO NÃO ENCONTRADO!\n");
+        return;
+    }
+
+
+    char linha[100];
+    char nomeTime[50];
+    int id;
+    int linhasCarregadas = 0;
+
+
+    while (fgets(linha, sizeof(linha), arquivo) != NULL)
+
+        if (sscanf(linha, "%d | %49[^\n]", &id, nomeTime) == 2)
+        {
+            if(totalEquipas < 50)
+            {
+                equipas[totalEquipas].id = id;
+                strcpy(equipas[totalEquipas].nome, nomeTime);
+                totalEquipas++;
+                linhasCarregadas++;
+
+                if (id >= proximoID)
+                {
+                    proximoID= id + 1;
+                }
+            }
+        }
+    fclose(arquivo);
+    if (linhasCarregadas == 1)
+    {
+        printf("\n\n\n                          ✅ %d equipa carregada!\n", linhasCarregadas);
+    }
+    else if (linhasCarregadas >= 2)
+    {
+        printf("\n\n\n                          ✅ %d equipas carregadas!\n", linhasCarregadas);
+
+    }
+    else 
+    {
+        printf("                                ❗ Ficheiro vazio.\n");
+
+    }
+    sleep(2);
+
+
+
+}
+
+
+ 
 
 //limpa a tela
 void limparTela()
@@ -63,13 +130,14 @@ void menuPrincipal()
     printf("║     ██ ▀▀ ██ ██▄▄   ██ ▀▄██ ██  ██   ██ ██ ▀▄██ ██ ██     ██ ██▄▄██ ██         ║\n");
     printf("║     ██    ██ ██▄▄▄▄ ██   ██ ▀████▀   ██ ██   ██ ██ ▀█████ ██ ██  ██ ██████     ║\n");
     printf("╚════════════════════════════════════════════════════════════════════════════════╝\n\n\n");
-    printf("                          ┎━─━─━─━─━─━─━─━─━─━─━─━─━─━─┒                         \n");
-    printf("                            1 - GESTÃO DE EQUIPAS                             \n");
-    printf("                            2 - REGISTRO DE JOGOS                            \n");
-    printf("                            3 - CÁLCULAR CLASSIFICAÇÃO                             \n");
-    printf("                            4 - ESTATÍSTICAS                                 \n");
-    printf("                            5 - SALVAR                                       \n");
-    printf("                            0 - SAIR                                         \n");
+    printf("                          ┎━─━─━─━─━─━─━─━─━─━─━─━─━─━─┒                          \n");
+    printf("                            1 - GESTÃO DE EQUIPAS                                 \n");
+    printf("                            2 - REGISTRO DE JOGOS                                 \n");
+    printf("                            3 - CÁLCULAR CLASSIFICAÇÃO                            \n");
+    printf("                            4 - ESTATÍSTICAS                                      \n");
+    printf("                            5 - SALVAR                                            \n");
+    printf("                            6 - CARREGAR DADOS                                    \n");
+    printf("                            0 - SAIR                                              \n");
     printf("                          ┖━─━─━─━─━─━─━─━─━─━─━─━─━─━─┚                          \n\n");
     printf("                          Selecione uma opção ➤  ");
 }
@@ -131,12 +199,15 @@ void cadastrarEquipa()
         {
             if (opcaoConf == 1)
             {
-                equipas[totalEquipas].id = totalEquipas + 1;
+                equipas[totalEquipas].id = proximoID;
                 strcpy(equipas[totalEquipas].nome, equipaNome);
                 totalEquipas++;
+                proximoID++; 
+
                 printf("\n\n                          ✅ Equipa cadastrada com sucesso!\n");
                 printf("                                      AGUARDE....\n");
-                sleep(2); // Alterado de Sleep(2000) para sleep(2)
+                sleep(2);
+
                 tentarNovamente = 0;
             }
             else if (opcaoConf == 2)
@@ -173,6 +244,7 @@ void gerirEquipas()
             while (getchar() != '\n');
         }
 
+
         switch (opcaoGest)
         {
         case 1:
@@ -206,15 +278,19 @@ void gerirEquipas()
 }
 
 // Inicia o programa e controla fluxo principal
-int main() {
+int main()
+
+{
+    carregarDados(); 
+
     int opcaoMenu;
 
     do {
         menuPrincipal();
-        while (scanf("%d", &opcaoMenu) != 1 || opcaoMenu < 0 || opcaoMenu > 5)
+        while (scanf("%d", &opcaoMenu) != 1 || opcaoMenu < 0 || opcaoMenu > 6)
         {
             menuPrincipal();
-            printf("\n\n                          Digite uma opção válida! (0 a 5)\n\n");
+            printf("\n\n                          Digite uma opção válida! (0 a 6)\n\n");
             printf("                          Selecione uma opção: ");
             while (getchar() != '\n');
         }
@@ -223,7 +299,8 @@ int main() {
         switch (opcaoMenu)
         {
         case 0:
-            printf("                             Finalizando o programa...\n");
+        
+            printf("                          Finalizando o programa...\n");
             sleep(2);
             limparTela();
             printf("\n\n                          Pressione Enter para continuar...\n\n");
@@ -249,8 +326,30 @@ int main() {
         case 5:
             salvarDados();
             break;
+        case 6:
+            carregarDados();
+            break;
         }
     } while (opcaoMenu != 0);
 
     return 0;
 }
+
+
+
+
+
+// precisamos ver se o campeonato é em forma de fae de grupos
+//jogos fora de casa jogos dentro de casa
+//combinar as equipas com os jogos
+//jogos tem que ter id junto com jogos
+//equipas com id, todos int
+//diagrama de objetos
+//escrever o resultado no id do jogo, numeros de gols do time dentro de casa e de fora
+//salvar os times num documento e resultados jogos em outro jogos.txt equipas.txt
+//criar uma sctruct para jogos?
+//selecionar de forma random nos arrays
+//pode ser de eliminação direto pra já (mais facil)
+//igual a copa do mundo por exemplo
+//muito mais facil
+
