5dae3b01fc222b72b1685441e5b4baf3c546ec0a
aironguimass <aironpablo@hotmail.com>
2026-01-10 15:51:33 +0000

    Improve output formatting in listarClassificacao
    Added a call to limparTela() for better output formatting.

--- lega/src/classificacao.c
+++ lega/src/classificacao.c
@@ -11,7 +11,7 @@
 
     void listarClassificacao() {
 
-
+limparTela();
         Equipa ranking[16];
         memcpy(ranking, equipas, sizeof(Equipa) * totalEquipas);
 
@@ -103,3 +103,4 @@
         getchar(); // Limpa buffer
         getchar(); // Espera tecla
     }
+
