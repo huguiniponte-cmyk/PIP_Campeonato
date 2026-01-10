73f20e604f730c243e6ef511ff9e9b5f70360d06
aironguimass <aironpablo@hotmail.com>
2026-01-10 05:01:16 +0000

    Add files via upload

---
+++ lega/include/classificacao.h
@@ -0,0 +1,12 @@
+#ifndef CLASSIFICACAO_H
+#define CLASSIFICACAO_H
+
+#include "equipas.h"
+
+// Função principal solicitada no menu (Opção 3)
+void listarClassificacao(void);
+
+// Funções auxiliares de ordenação
+void classificarPorDesempenho(Equipa temp[], int n);
+
+#endif
